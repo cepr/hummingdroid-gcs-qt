3a6134c51acb9d42a9c6704944f32d99a4ba434e
Cedric Priscal <cedric.priscal@gmail.com>
2013-09-22 17:12:23 -0700

    Modifying graph colors

--- src/plotarea.cpp
+++ src/plotarea.cpp
@@ -5,11 +5,11 @@
 PlotArea::PlotArea(QWidget *parent) :
     QLabel(parent)
 {
-    pens[CURRENT].setColor(QColor(255, 0, 0));
+    pens[CURRENT].setColor(QColor(0, 0, 0));
     pens[CURRENT].setWidth(3);
-    pens[COMMAND].setColor(QColor(0, 255, 0));
+    pens[COMMAND].setColor(QColor(0, 0, 255));
     pens[COMMAND].setWidth(3);
-    pens[MOTOR].setColor(QColor(0, 0, 255));
+    pens[MOTOR].setColor(QColor(255, 0, 0));
     pens[MOTOR].setWidth(3);
 
     memset(values, 0, sizeof(values));
